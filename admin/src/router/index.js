5ecffc5d9c1ad61176f8fc5c64a84f7ec07279bc
binyang <bybookcover@hotmail.com>
2022-09-12 22:52:19 +0800

    广告位

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -10,6 +10,13 @@ import ItemList from '../views/ItemList.vue'
 
 import HeroEdit from '../views/HeroEdit.vue'
 import HeroList from '../views/HeroList.vue'
+
+
+import ArticleEdit from '../views/ArticleEdit.vue'
+import ArticleList from '../views/ArticleList.vue'
+
+import AdEdit from '../views/AdEdit.vue'
+import AdList from '../views/AdList.vue'
 Vue.use(Router)
 
 const routes = [
@@ -57,6 +64,33 @@ const routes = [
       {
         path:'/heros/edit/:id',
         component: HeroEdit,
+        props: true
+      },
+      {
+        path:'/articles/create',
+        component: ArticleEdit
+      },
+      {
+        path:'/articles/list',
+        component: ArticleList
+      }, 
+      {
+        path:'/articles/edit/:id',
+        component: ArticleEdit,
+        props: true
+      },
+
+      {
+        path:'/ads/create',
+        component: AdEdit
+      },
+      {
+        path:'/ads/list',
+        component: AdList
+      }, 
+      {
+        path:'/ads/edit/:id',
+        component: AdEdit,
         props: true
       },
     ]
--- admin/src/views/ArticleList.vue
+++ admin/src/views/ArticleList.vue
@@ -1,24 +1,18 @@
 <template>
   <div>
-    <h1>英雄列表</h1>
+    <h1>文章列表</h1>
       <el-table :data="items">
           <el-table-column prop="_id" label="ID" width="240">
           </el-table-column>
-          <el-table-column prop="name" label="英雄名称">
+          <el-table-column prop="title" label="标题">
           </el-table-column>
-          <el-table-column prop="title" label="称号">
-          </el-table-column>
-          <el-table-column prop="avatar" label="头像">
-            <template slot-scope="scope">
-              <img :src="scope.row.avatar" alt="" style="height:3rem;">
-            </template>
-          </el-table-column>
+
           <el-table-column
             fixed="right"
             label="操作"
             width="180">
             <template slot-scope="scope">
-              <el-button type="text" size="small" @click="$router.push(`/heros/edit/${scope.row._id}`)">编辑</el-button>
+              <el-button type="text" size="small" @click="$router.push(`/articles/edit/${scope.row._id}`)">编辑</el-button>
               <el-button type="text" size="small" @click="remove(scope.row)">删除</el-button>
             </template>
           </el-table-column>
@@ -34,16 +28,16 @@
     },
     methods:{
       async fetch(){
-         const res = await this.$http.get('rest/heros')
+         const res = await this.$http.get('rest/articles')
         this.items =res.data
       },
       async remove(row){
-        this.$confirm(`是否确定要删除分类 ${row.name}`, '提示', {
+        this.$confirm(`是否确定要删除文章 ${row.title}`, '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then( async () => {
-          const res = await this.$http.delete(`rest/heros/${row._id}`)
+          const res = await this.$http.delete(`rest/articles/${row._id}`)
           this.$message({
             type: 'success',
             message: '删除成功!'
