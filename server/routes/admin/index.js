bf07688fb8f9b1a37f87662af20711a64da46731
binyang <bybookcover@hotmail.com>
2022-09-14 12:40:52 +0800

    aa

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -44,4 +44,8 @@ module.exports = app =>{
         file.url= `http://localhost:3000/uploads/${file.filename}`
         res.send(file)
     })
+
+    app.post('/admin/api/login',async(req,res) =>{
+        res.send('ok')
+    })
 }
